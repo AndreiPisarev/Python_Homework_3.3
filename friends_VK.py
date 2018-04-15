19329c648117b402e5c90428a8c4ee43ddd912ad
AndreiPisarev <berky.87@gmail.com>
2018-04-15 14:21:38 +0300

    Использовал 'target_uids' вместо 'target_uid'

--- friends_VK.py
+++ friends_VK.py
@@ -7,7 +7,7 @@ APP_ID = 6447963
 VERSION = '5.74'
 
 source_uid = 286048734  # ID пользователя, чьи друзья пересекаются с друзьями пользователя с идентификатором target_uid
-target_uid = 124491235  # ID пользователя, с которым необходимо искать общих друзей.
+target_uids = [124491235, 247244194]   # ID пользователей, с которым необходимо искать общих друзей.
 
 token = '43a0837ae955fb0b72e540f121d14dd8a02501952dca6d4596c9ccc6e3e9483012f5a6d7cfe8bdb469eb0'
 
@@ -21,31 +21,33 @@ def authorize_vk():
     }
     print('Перейдите по ссылке для получения токена (скопируйте токен и вставте в поле ввода)')
     print('?'.join((AUTH_URL, urlencode(auth_data))))
-    token1 = input('Вставьте токен:')
+    token_input = input('Вставьте токен:')
 
-    return token1
+    return token_input
 
 
-def mutual_friends(source_uid, target_uid, token):
-    """Для поиска общих друзей используем метод getMutual объекта friends"""
+def mutual_friends(source_uid, target_uids, token):
+    """Для поиска общих друзей используем метод getMutual объекта friends,
+    использовали параметр target_uids для сравнения c ID"""
 
     params = {
         'v': VERSION,
         'access_token': token,
         'source_uid': source_uid,
-        'target_uid': target_uid
+        'target_uids': target_uids
     }
 
     response = requests.get('https://api.vk.com/method/friends.getMutual', params)
+    print(response.json())
     response_dict = response.json()
-    list_friends = response_dict['response']
+    list_friends = response_dict['response'][0]['common_friends']
 
-    print('Список общих друзей для ID-{} и ID-{}:'.format(source_uid, target_uid))
+    print('Список общих друзей для ID-{} и ID-{}:'.format(source_uid, target_uids))
     for friend in list_friends:
         print('https://vk.com/id{}'.format(friend))
 
 
 # token = authorize_vk()
-mutual_friends(source_uid, target_uid, token)
+mutual_friends(source_uid, target_uids, token)
 
 
